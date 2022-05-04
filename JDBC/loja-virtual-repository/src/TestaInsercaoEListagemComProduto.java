274423d94750681af662840c379082d1c3825fdc
Arthur Solla <arthursolla1@outlook.com>
2022-05-04 15:11:25 -0300

    Atualizando curso de JDBC

---
+++ JDBC/loja-virtual-repository/src/TestaInsercaoEListagemComProduto.java
@@ -0,0 +1,19 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+public class TestaInsercaoEListagemComProduto {
+
+	public static void main(String[] args) throws SQLException {
+		
+		Produto comoda = new Produto("Comoda", "Comoda vertical");
+		
+		try(Connection connection = new ConnectionFactory().recuperarConexao()){
+			ProdutoDAO produtoDao = new ProdutoDAO(connection);
+			produtoDao.salvar(comoda);
+			List<Produto> listaDeProdutos = produtoDao.listar();
+			listaDeProdutos.stream().forEach(lp -> System.out.println(lp));
+		}
+	}
+	
+}
