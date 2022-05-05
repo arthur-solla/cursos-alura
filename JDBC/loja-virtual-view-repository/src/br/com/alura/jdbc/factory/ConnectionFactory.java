02aef74501e6599e6eac4f0d295a87ef0c494d83
Arthur Solla <arthursolla1@outlook.com>
2022-05-05 13:00:55 -0300

    Atualizando curso JDBC

--- JDBC/loja-virtual-view-repository/src/br/com/alura/jdbc/factory/ConnectionFactory.java
+++ JDBC/loja-virtual-view-repository/src/br/com/alura/jdbc/factory/ConnectionFactory.java
@@ -1,3 +1,4 @@
+package br.com.alura.jdbc.factory;
 import java.sql.Connection;
 import java.sql.SQLException;
 
@@ -6,7 +7,7 @@ import javax.sql.DataSource;
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 public class ConnectionFactory {
-	
+
 	public DataSource dataSource;
 
 	public ConnectionFactory() {
@@ -14,14 +15,15 @@ public class ConnectionFactory {
 		comboPooledDataSource.setJdbcUrl("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC");
 		comboPooledDataSource.setUser("root");
 		comboPooledDataSource.setPassword("root");
-		
-		comboPooledDataSource.setMaxPoolSize(15);
-		
+
 		this.dataSource = comboPooledDataSource;
 	}
-	
-	public Connection recuperarConexao() throws SQLException {
-		return this.dataSource.getConnection();
+
+	public Connection recuperarConexao() {
+		try {
+			return this.dataSource.getConnection();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
 	}
-	
 }
